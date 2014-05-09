74afdd6d54edd7ed89ec905a245c9df501955724
Farjad <qazi.farjad@gmail.com>
2014-05-09 09:14:41 +0500

    Project Skeletal Structure Uploaded (Incomplete) -Contains: 	Beans to interact with database 	dbCommxxx (Database Communication) java classes that we will be using for creating and retrieving 	database entries

---
+++ CourseIT_Proj/src/java/webProj/CourseIT/Beans/CoursesBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package webProj.CourseIT.Beans;
+
+/**
+ *
+ * @author Farjad
+ */
+public class CoursesBean {
+    private int Course_ID;
+    private String Course_Name;
+    private String Instructor;
+    private int Course_UV;
+    private String Course_pic;
+    
+    //TODO: Getter Setter Functions
+}
---
+++ CourseIT_Proj/src/java/webProj/CourseIT/Beans/InternshipsBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package webProj.CourseIT.Beans;
+
+/**
+ *
+ * @author Farjad
+ */
+public class InternshipsBean {
+    	private int internship_id;
+	private String submitter;
+	private String info;
+        
+        //TODO: Setter/Getter Functions.
+}
