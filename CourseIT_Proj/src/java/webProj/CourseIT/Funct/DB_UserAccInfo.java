f7d2b8752617c9b93210decd03583600d8543a8a
Talal Saleem <12bscsmtsaleem@seecs.edu.pk>
2014-05-12 14:22:08 +0500

    login- error

--- CourseIT_Proj/src/java/webProj/CourseIT/Funct/DB_UserAccInfo.java
+++ CourseIT_Proj/src/java/webProj/CourseIT/Funct/DB_UserAccInfo.java
@@ -4,12 +4,47 @@
  * and open the template in the editor.
  */
 
+
 package webProj.CourseIT.Funct;
 
+
+import java.util.Date;
+import java.util.Iterator; 
+import java.util.List; 
+import org.hibernate.HibernateException; 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import webProj.CourseIT.Beans.Useraccinfo;
 /**
  *
  * @author Talal Saleem
  */
 public class DB_UserAccInfo {
+       private static SessionFactory factory;
+       
+       
+          /* Method to CREATE an employee in the database */
+   public Integer CheckUser(String email, String password){
+      Session session = factory.openSession();
+      Transaction tx = null;
+      Integer userID = null;
+      try{
+         tx = session.beginTransaction();
+      Useraccinfo u =  new Useraccinfo();
+      Query query = session.getNamedQuery("Useraccinfo.findByEmail");
+              query.setParameter("email", email);
+              int id = query
+         tx.commit();
+      }catch (HibernateException e) {
+         if (tx!=null) tx.rollback();
+         e.printStackTrace(); 
+      }finally {
+         session.close(); 
+      }
+      return userID;
+   }
     
 }
