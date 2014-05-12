a87da96865da887d8f6791519173a8ecb56f278c
Talal Saleem <12bscsmtsaleem@seecs.edu.pk>
2014-05-12 13:22:16 +0500

    Funct Update

---
+++ CourseIT_Proj/src/java/webProj/CourseIT/Funct/DB_CourseEnroll.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package webProj.CourseIT.Funct;
+
+/**
+ *
+ * @author Talal Saleem
+ */
+public class DB_CourseEnroll {
+    
+}
